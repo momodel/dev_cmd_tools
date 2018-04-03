bf7fe1095be0699b12c3e3565544efc806ac07f2
Chun <xxx@sxx.com>
2018-04-04 00:36:53 +0800

    test

--- momodel.py
+++ momodel.py
@@ -10,7 +10,7 @@ from cement.core.controller import CementBaseController, expose
 
 class ValidationController(CementBaseController):
 
-#test
+#testasdfasdfadsfdsf
     class Meta:
 
         label = 'base'
